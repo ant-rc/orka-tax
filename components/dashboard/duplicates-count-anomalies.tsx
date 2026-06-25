1d1b8b087cab5477a99163d7216099af77d63183
EbMian <127929560+EbMian@users.noreply.github.com>
2026-06-25 22:35:07 +0200

    fix(duplicates-count-anomalies): design according to the Figma model

--- components/dashboard/duplicates-count-anomalies.tsx
+++ components/dashboard/duplicates-count-anomalies.tsx
@@ -7,11 +7,11 @@ export default function DuplicatesCountAnomalies({
 
 }: DuplicatesCountAnomaliesProps) {
   return (
-    <div className="px-5 py-3 flex items-center gap-2 flex-wrap text-sm border-b border-ui-border duplicates-count-bar">
+    <div className="px-5 py-3 flex items-center gap-2 flex-wrap text-sm border-b border-ui-border duplicates-count-bar mb-4">
       <img src="/assets/warning-badge.svg" className=""/>
       <span className="ms-2 warning-text">...</span>
       <span className="text-xs font-semibold text-ui-text-highlighted me-2 warning-text">doublons potentiels détectés</span>
-      <span className="underline text-xs warning-text">Voir et résoudre</span>
+      <button className="underline warning-text text-xs see-and-resolve">Voir et résoudre</button>
     </div>
 
   );
