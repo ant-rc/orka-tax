301d5232c8cdb59a1c7dcb72895d59b374278c56
EbMian <127929560+EbMian@users.noreply.github.com>
2026-06-25 21:31:37 +0200

    fix(statsbar-anomalies): design according to the Figma model

--- components/dashboard/statsbar-anomalies.tsx
+++ components/dashboard/statsbar-anomalies.tsx
@@ -6,8 +6,8 @@ interface StatsbarAnomaliesProps {
 export default function StatsbarAnomalies({
 }: StatsbarAnomaliesProps) {
   return (
-    <div className="stats-bar-anomalies grid grid-cols-3 flex items-center flex-wrap gap-6 px-5 py-3 border-b border-ui-border">
-      <div className="flex justify-between flex items-center">
+    <div className="stats-bar-anomalies grid grid-cols-3 flex items-center flex-wrap gap-6 border-ui-border">
+      <div className="flex justify-between flex items-center flex-1 overflow-auto bg-white border border-ui-border rounded-lg px-5 py-3">
         <div className="flex items-center gap-2">
           <img src="/assets/warning.svg" className=""/>
           <span className="ms-2">Anomalies à qualifier</span>
@@ -15,7 +15,7 @@ export default function StatsbarAnomalies({
         <span className="ms-2 warning-text">...</span>
       </div>
 
-      <div className="flex justify-between flex items-center">
+      <div className="flex justify-between flex items-center flex-1 overflow-auto bg-white border border-ui-border rounded-lg px-5 py-3">
         <div className="flex items-center gap-2">
           <img src="/assets/chart.svg" className=""/>
           <span className="ms-2">Taux de qualification</span>
@@ -26,7 +26,7 @@ export default function StatsbarAnomalies({
         </span>
       </div>
       
-      <div className="flex justify-between flex items-center">
+      <div className="flex justify-between flex items-center flex-1 overflow-auto bg-white border border-ui-border rounded-lg px-5 py-3">
         <div className="flex items-center gap-2">
           <img src="/assets/argent.webp" className=""/>
           <span className="ms-2">Montant récupérable restant</span>
